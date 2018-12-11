a3171a0248fe6b1e47298d1b4fb81df150ee4e3f
Abhinav <abhinav95@hotmail.com>
2018-12-12 00:45:31 +0530

    Initial Commit

---
+++ beginner-exercises/src/components/01-HelloWorld.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Task: Render "Hello World" text.
+// class HelloWorld extends Component {
+//   // All components *must* have a `render` method defined.
+
+//   // To define a component's render method, we use syntax called JSX. As you
+//   // can see it looks similar to HTML. JSX gets converted to
+//   // JavaScript code. It is here just for readability purposes.
+
+//   render() {
+//     return (
+//       <h1>Render method is a must when a component is Created</h1>
+//     );
+//   }
+// }
+
+const HelloWorld = () => (<h1>Render method is a must when a component is Created</h1>) ;
+
+export default HelloWorld;
